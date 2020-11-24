103490e1d29851a6e4d2e82acd829a40f8306b24
Andrey <zylkovsliy.andrey@mail.ru>
2020-11-24 20:45:00 +0300

    Created solutions

--- lesson_6/task_1.py
+++ lesson_6/task_1.py
@@ -1,8 +1,10 @@
 import sys
+from copy import copy
 
 
 def _get_obj_memory_usage(obj, done):
-    """Returns the amount of memory used by the object, including nested elements, if the object is a collection"""
+    if hasattr(obj, '__call__') or hasattr(obj, '__loader__'):
+        return 0
 
     total = sys.getsizeof(obj)
     done.add(id(obj))
@@ -22,7 +24,8 @@ def _get_obj_memory_usage(obj, done):
 
 def calc_memory(namespace):
     total = 0
-    done = set()  # Не все переменные ведут к уникальным объектам
+    done = set()
+
     for value, obj in namespace.items():
         # на случай, если в качестве namespace был передан словарь globals()
         if value.startswith('__') and value.endswith('__'):
@@ -34,10 +37,10 @@ def calc_memory(namespace):
             msg = "repeated"
         else:
             mem = _get_obj_memory_usage(obj, done)
-            msg = f"{mem} bytes"
+            msg = f"type '{type(obj).__name__}' {mem} bytes"
             total += mem
 
-        print(f'{value}:', msg)
+        print(f'{value}: {msg}')
     print(f'Total memory usage: {total} bytes')
 
 
@@ -59,6 +62,23 @@ def solution_1(sequence):
     return first_min_ind, first_min_ind
 
 
+def solution_2(sequence):
+    sequence_c = copy(sequence)
+
+    min_1 = sequence_c.index(min(sequence_c))
+    sequence_c.pop(min_1)
+    min_2 = sequence_c.index(min(sequence_c))
+
+    calc_memory(locals())
+    return min_1, min_2
+
+
+def solution_3(sequence):
+    sorted_seq = sorted(enumerate(sequence), key=lambda x: x[1])
+    calc_memory(locals())
+    return sorted_seq[0][0], sorted_seq[1][0]
+
+
 if __name__ == '__main__':
     from random import randint
 
@@ -67,12 +87,13 @@ if __name__ == '__main__':
     MAX_ITEM = 100
 
     array = [randint(MIN_ITEM, MAX_ITEM) for _ in range(ITEMS)]
-    funcs = (solution_1,)  # , solution_2, solution_3)
+    funcs = (solution_1, solution_2, solution_3)
+
+    print('Global namespace', end='\n\n')
+    calc_memory(globals())
+    print()
 
     for f in funcs:
         print('Function', f.__name__, end='\n\n')
         f(array)
-    print()
-
-    print('Global namespace', end='\n\n')
-    calc_memory(globals())
+        print()
